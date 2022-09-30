43da28d522287f8e30d660812e496a4b90b153df
Simone Sun <sunx660@newschool.edu>
2022-09-30 10:10:17 -0400

    Initial push

---
+++ week03/dance-party/script.js
@@ -0,0 +1,53 @@
+
+// loading music tutorial from https://nishanc.medium.com/audio-visualization-in-javascript-with-p5-js-cf3bc7f1be07
+// easing tutorial from https://p5js.org/examples/input-easing.html
+
+let song, amp;
+let circlingBall, ampBall, lines, mybox;
+let bouncingBalls = [];
+
+function preload() {
+  // preload audio
+  song = loadSound("audio/giorgui-cut.mp3");
+}
+
+function setup() {
+  createCanvas(325, 325, WEBGL);
+
+  // loop audio
+  song.loop();
+  amp = new p5.Amplitude();
+
+  //Create objects
+  for (let i = 0; i < 10; i++) {
+    bouncingBalls.push(new Ball());
+  }
+  circlingBall = new Ball();
+  ampBall = new Ball();
+  myBox = new Box();
+  lines = new LineGroup(40, 200);
+}
+
+function draw() {
+  background(0);
+
+  let vol = amp.getLevel();
+
+  // operate object methods
+  bouncingBalls.forEach((ball) => {
+    ball.bounce();
+    ball.display();
+  });
+  circlingBall.circle(vol);
+  circlingBall.display();
+  ampBall.amplify(vol);
+  ampBall.display();
+  lines.amplify(vol);
+  lines.display();
+  myBox.rotate();
+  myBox.display();
+}
+
+// function touchStarted() {
+//   getAudioContext().resume();
+// }
--- week03/dd/script.js
+++
@@ -1,9 +0,0 @@
-
-function setup() {
-  createCanvas(325, 325);
-  
-}
-
-function draw(){
-  
-}

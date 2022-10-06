03d03223f5397e6dbd4be5fba3756636a623dac8
Simone Sun <sunx660@newschool.edu>
2022-10-06 17:00:35 -0400

    Use p5.scribble library

--- week04/pond/scripts/main.js
+++ week04/pond/scripts/main.js
@@ -1,24 +1,27 @@
+let s;
 let waterColor, leafColor;
 let ripples = [];
 
 function setup() {
   createCanvas(400, 400);
+  s = new Scribble();
+
   waterColor = color(182, 201, 185);
   leafColor = color(90, 140, 127);
 }
 
 function draw() {
   background(waterColor);
-
+  frameRate(5);
   ripples.forEach((ripple) => {
     ripple.display();
   });
 
   noStroke();
   fill(leafColor);
-  circle(100, 200, 50);
+  s.scribbleEllipse(100, 200, 50, 50);
   fill(waterColor);
-  arc(100,200, 50,50, PI + QUARTER_PI, PI+HALF_PI);
+  arc(100,200, 55, 55, PI + QUARTER_PI, PI+HALF_PI);
 }
 
 function mousePressed() {
--- week04/pond/scripts/ripple.js
+++ week04/pond/scripts/ripple.js
@@ -11,19 +11,19 @@ class Ripple {
     //stop looping when the ripple is out of canvas frame
     if (this.outerDiam < width * 3) {
       for (let i = 0; i < 5; i++) {
-        let diam = this.outerDiam - (10 + i * 2) * i;
+        let diam = this.outerDiam - (20 + i * 3) * i;
         if (diam > 0) {
           // the bigger the circle gets, the closer its color is too the waterColor: color(182,201,185); starting from color(87,117,91)
           let fadeR = map(diam, 0, width * 1, 87, 182);
           let fadeG = map(diam, 0, width * 1, 117, 201);
           let fadeB = map(diam, 0, width * 1, 91, 185);
           stroke(fadeR, fadeG, fadeB);
-          strokeWeight(0.5);
+          strokeWeight(2);
           noFill();
-          ellipse(this.x, this.y, diam);
+          s.scribbleEllipse(this.x, this.y, diam, diam);
         }
       }
-      this.outerDiam = this.outerDiam + 2;
+      this.outerDiam = this.outerDiam + 20;
     }
   }
 }

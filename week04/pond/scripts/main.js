4a140730ca3f9fb87c3f3574eb5d10b1100bbde5
Simone Sun <sunx660@newschool.edu>
2022-10-06 17:59:16 -0400

    Add detectCollision()
    still need improvement

--- week04/pond/scripts/main.js
+++ week04/pond/scripts/main.js
@@ -14,6 +14,8 @@ function setup() {
   for(let i = 0; i < 10; i ++){
     leaves.push(new Leaf());
   }
+    console.log(leaves);
+  detectCollision();
 }
 
 function draw() {
@@ -22,8 +24,6 @@ function draw() {
   ripples.forEach((ripple) => {
     ripple.display();
   });
-
-  }
 
   leaves.forEach((leaf) => {
     leaf.display();
@@ -41,5 +41,17 @@ function makeNewRipple() {
 }
 
 function detectCollision(){
-
+  for(let i = 0; i < leaves.length-1; i ++){
+    for(let j = i+1; j < leaves.length-1; j++){
+      // get distance between two leaves
+      var d = dist(leaves[i].x, leaves[i].y, leaves[j].x, leaves[j].y);
+      // there is collision if distance between two is less than sum of two radius
+      if (d <= leaves[i].d/2 + leaves[j].d/2) {
+        console.log(`collide ${i}`);
+        // remove objects that collide
+        leaves.splice(j,j);
+      }
+    }
+  }
+  console.log(leaves);
 }

f7ef9a7a1dba59f6e38a41bbba396bc3b767f21a
Simone Sun <sunx660@newschool.edu>
2022-10-15 21:38:02 -0400

    Setup for Server Sent Event

---
+++ week05/emoji Tracker/scripts/main.js
@@ -0,0 +1,49 @@
+// this app uses emojiTracker STREAM API.
+// Unlike the REST API, it constantly receives events as long as the connection is open
+// So instead of doing fetch(), I'm using EventSource.onmessage
+// for more about SSE (server-sent events), visit https://developer.mozilla.org/en-US/docs/Web/API/EventSource
+
+// Emojitracker Streaming API: https://github.com/emojitracker/emojitrack-streamer-spec/blob/master/README.md
+
+// log all tweets contsins the emoji
+// emoji by codepoint https://emojipedia.org/emoji/
+const endpoint = "https://stream.emojitracker.com/subscribe/eps";
+const evtSource = new EventSource(endpoint);
+let sob = 0;
+
+// get DOM
+const eventList = document.querySelector("ul");
+
+evtSource.onmessage = (e) => {
+  // parse json
+  let response = JSON.parse(e.data);
+  // do something
+  for (const key in response) {
+    if (Object.hasOwnProperty.call(response, "1F62D")) {
+      sob++;
+    }
+  }
+
+  // console.log(response.1F602);
+  // console.log(e.data);
+};
+
+// evtSource.addEventListener("message", (e) => {
+//   // const newElement = document.createElement("li");
+//   // newElement.textContent = `message: ${e.data}`;
+//   // eventList.appendChild(newElement);
+//   // const update = JSON.parse(e.data);
+//   // console.log(`${update.screen_name} tweeted ${update.text}`);
+//   console.log(`message: ${e.data}`);
+// });
+
+function setup() {
+  let cnv = createCanvas(windowWidth * 0.8, windowHeight * 0.8);
+  cnv.position(windowWidth * 0.1, windowHeight * 0.2);
+  background("black");
+}
+function draw() {
+  // console.log(element);
+  rectMode(LEFT);
+  rect(0, 15, sob / 10, 15);
+}

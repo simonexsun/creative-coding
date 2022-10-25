d813155a7aabd1484f3c49fb2c3e145fbe7d1982
Simone Sun <sunx660@newschool.edu>
2022-10-24 20:07:58 -0400

    Refine file structure

---
+++ midterm/music-box/scripts/line.js
@@ -0,0 +1,28 @@
+class LineGroup {
+  constructor(length, number) {
+    this.x = 0; // Xposition of signle line ending point
+    this.y = 0; // Yposition of signle line ending point
+    this.l = length; // single line length
+    this.offset = 1;
+    this.n = number; // number of bars
+    this.colorGold = color(215, 190, 105);
+  }
+
+  amplify(vol) {
+    let intensity = 120;
+    this.offset = vol * intensity;
+    // this.l = vol * intensity;
+  }
+
+  display() {
+    noFill();
+    strokeWeight(0.5);
+    stroke(this.colorGold);
+    for (let i = 0; i < this.n; i++) {
+      // push();
+      rotate(radians((360 / this.n) * i));
+      line(0, this.offset + this.l, 0, this.offset);
+      // pop();
+    }
+  }
+}
--- midterm/music-box/scripts/main.js
+++ midterm/music-box/scripts/main.js
@@ -1,5 +1,6 @@
 // loading music tutorial from https://nishanc.medium.com/audio-visualization-in-javascript-with-p5-js-cf3bc7f1be07
 // easing tutorial from https://p5js.org/examples/input-easing.html
+// sound tracks from artist Helios, aquired from http://unseen-music.com/yume/loops/trimmed/
 
 let song, amp;
 let circlingBall, ampBall, lines, mybox;
@@ -33,19 +34,18 @@ function setup() {
 
 function draw() {
   background(0);
-  
+
   //display text if the music is not playing
-  if(getAudioContext().state=='suspended'){
+  if (getAudioContext().state == "suspended") {
     push();
     textFont(myFont);
     textSize(20);
-    fill('white');
+    fill("white");
     textAlign(CENTER, BOTTOM);
-    text('click anywhere to play music', -width/2, height*0.4, width);
+    text("click anywhere to play music", -width / 2, height * 0.4, width);
     pop();
   }
 
-  
   let vol = amp.getLevel();
 
   // operate object methods
@@ -63,7 +63,7 @@ function draw() {
 }
 
 // Chrome 70 will require user gestures to enable web audio api > https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
-// Click on the web page to start audio 
+// Click on the web page to start audio
 function touchStarted() {
-  getAudioContext().resume();   
+  getAudioContext().resume();
 }

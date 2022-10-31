d9184b97983a2f9eda9a4e54453bbd1841df5c11
Simone Sun <sunx660@newschool.edu>
2022-10-31 12:04:30 -0400

    Update circlingBallUI default value

--- midterm/music-box/scripts/UI.js
+++ midterm/music-box/scripts/UI.js
@@ -121,8 +121,9 @@ class CirclingBallUI extends UI {
 
     this.R = R;
     this.r = r;
-    this.ellipseX = x + R + r;
-    this.ellipseY = y;
+    this.ellipseY = y + R * 0.8;
+    this.ellipseX =
+      this.x + sqrt(sq(this.R + this.r) - sq(this.y - this.ellipseY));
     this.color = color(169, 215, 105); // green
   }
 

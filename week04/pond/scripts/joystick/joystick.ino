bb1382ea99cc80e89033c550db5a1c69a379ca7e
Simone Sun <sunx660@newschool.edu>
2022-11-25 11:14:34 -0500

    Update joystick serial output format

--- week04/pond/scripts/joystick/joystick.ino
+++ week04/pond/scripts/joystick/joystick.ino
@@ -10,31 +10,32 @@ int SW_state = 0;
 int mapX = 0;
 int mapY = 0;
 int offsetX = 22; // change this based out actuall joystick output
-int offsetY = 1; // change this based out actuall joystick output
+int offsetY = 1;  // change this based out actuall joystick output
 
-void setup() {
-  Serial.begin(9600); 
-  
+void setup()
+{
+  Serial.begin(9600);
+
   pinMode(VRx, INPUT);
   pinMode(VRy, INPUT);
-  pinMode(SW, INPUT_PULLUP); 
-  
+  pinMode(SW, INPUT_PULLUP);
 }
 
-void loop() {
+void loop()
+{
   xPosition = analogRead(VRx);
   yPosition = analogRead(VRy);
   SW_state = digitalRead(SW);
   mapX = map(xPosition, 0, 1023, -512, 512) + offsetX;
   mapY = map(yPosition, 0, 1023, -512, 512) + offsetY;
-  
-  Serial.print("X: ");
+
+  Serial.print("{x: ");
   Serial.print(mapX);
-  Serial.print(" | Y: ");
+  Serial.print(", y: ");
   Serial.print(mapY);
-  Serial.print(" | Button: ");
-  Serial.println(!SW_state); // reverse button state to: 1 when pressed, 0 when released
+  Serial.print(", button: ");
+  Serial.print(!SW_state); // reverse button state to: 1 when pressed, 0 when released
+  Serial.println("}");
 
   delay(100);
-  
 }

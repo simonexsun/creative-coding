9120f1738225fbcfdde9d663fea13e6195999b8b
Simone Sun <sunx660@newschool.edu>
2022-11-24 23:07:27 -0500

    Add functions for serial communication

--- week04/pond/scripts/main.js
+++ week04/pond/scripts/main.js
@@ -7,6 +7,12 @@ let ripples = [];
 let leaves = [];
 let frog, frogX, frogY, frogFound; // object variables
 
+// Serial variables
+let serial; // variable to hold an instance of the serialport library
+let portName = "/dev/tty.usbmodem1101"; // fill in your serial port name here
+let inData; // for incoming serial data (in the form of JSON)
+let portSelector;
+
 function preload() {
   // preload audio
   waterBloop1 = loadSound("./audios/water-bloop-1.mp3");
@@ -47,6 +53,17 @@ function setup() {
   frogY = leaves[2].y;
   frogSize = leaves[2].d * 0.6;
   frogFound = false;
+
+  // Serial setup
+  serial = new p5.SerialPort(); // make a new instance of the serialport library
+  serial.on("list", printList); // set a callback function for the serialport list event
+  serial.on("connected", serverConnected); // callback for connecting to the server
+  serial.on("open", portOpen); // callback for the port opening
+  serial.on("data", serialEvent); // callback for when new data arrives
+  serial.on("error", serialError); // callback for errors
+  serial.on("close", portClose); // callback for the port closing
+  serial.list(); // list the serial ports
+  serial.openPort(portName); // open a serial port
 }
 
 function draw() {
@@ -73,6 +90,8 @@ function draw() {
   leaves.forEach((leaf) => {
     leaf.display();
   });
+
+  console.log(inData);
 }
 
 function mousePressed() {
@@ -184,4 +203,53 @@ function displayUI() {
     width / 3,
     small_font_size + big_font_size + margine
   );
+}
+
+/* ==========================================================================
+Below are functions for serial communication */
+
+// make a serial port selector object:
+function printList(portList) {
+  // create a select object:
+  portSelector = createSelect();
+  portSelector.position(10, 10);
+  // portList is an array of serial port names
+  for (var i = 0; i < portList.length; i++) {
+    // add this port name to the select object:
+    portSelector.option(portList[i]);
+  }
+  // set an event listener for when the port is changed:
+  portSelector.changed(mySelectEvent);
+}
+
+function mySelectEvent() {
+  let item = portSelector.value();
+  // if there's a port open, close it:
+  if (serial.serialport != null) {
+    serial.close();
+  }
+  // open the new port:
+  serial.openPort(item);
+}
+
+function serverConnected() {
+  console.log("connected to server.");
+}
+
+function portOpen() {
+  console.log("the serial port opened.");
+}
+
+function serialEvent() {
+  // read a byte from the serial port, convert it to a number:
+  inString = serial.readLine();
+  inData = inString;
+}
+
+function serialError(err) {
+  console.log("Something went wrong with the serial port. " + err);
+}
+
+function portClose() {
+  console.log("The serial port closed.");
 }

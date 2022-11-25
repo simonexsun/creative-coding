11b94f3469009a39dc2a73965cebcbd7989d6858
Simone Sun <sunx660@newschool.edu>
2022-11-25 12:43:29 -0500

    Remove serial print list of ports

--- week04/pond/scripts/main.js
+++ week04/pond/scripts/main.js
@@ -66,13 +66,11 @@ function setup() {
 
   // Serial setup
   serial = new p5.SerialPort(); // make a new instance of the serialport library
-  serial.on("list", printList); // set a callback function for the serialport list event
   serial.on("connected", serverConnected); // callback for connecting to the server
   serial.on("open", portOpen); // callback for the port opening
   serial.on("data", serialEvent); // callback for when new data arrives
   serial.on("error", serialError); // callback for errors
   serial.on("close", portClose); // callback for the port closing
-  serial.list(); // list the serial ports
   serial.openPort(portName); // open a serial port
 }
 

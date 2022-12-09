45811f1772c1567ec51c3df43e483e513d39fefb
Simone Sun <sunx660@newschool.edu>
2022-12-09 13:17:20 -0500

    Update Arduino code

--- final/HallSensor_BLE/HallSensor_BLE.ino
+++ final/HallSensor_BLE/HallSensor_BLE.ino
@@ -1,28 +1,56 @@
-//Simple sketch to access the internal hall effect detector on the esp32.
-//values can be quite low.
-//Brian Degger / @sctv
+// Simple sketch to access the internal hall effect detector on the esp32.
+// Source code from Brian Degger / @sctv, https://www.hackster.io/lemio/create-apps-for-the-esp32-using-ble-through-p5-55292d
 
 // BleSerial library usage: https://github.com/avinabmalla/ESP32_BleSerial/blob/master/examples/BleSerial_Hello/bleserial_hello.ino
-#include "BLESerial.h"
+#include <BleSerial.h>
 
 int val = 0;
-BLESerial bleSerial;
+bool state;
+bool previousState;
+int counter = 0;
+BleSerial bleSerial;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   bleSerial.begin("ESP32");
+  state = false;
+  previousState = false;
 }
 
-void loop() {
+void loop()
+{
   val = hallRead();
-  Serial.print("sensor = ");
-  Serial.println(val);
+  // register hall sensor state as HIGH if value passes certain threadhold
+  if (val >= 100)
+  {
+    state = true;
+  }
+  else
+  {
+    state = false;
+  }
+  updateCounter();
 
-  //If we're connected
-  if (bleSerial.connected()) {
-    //Send the analog value
-    bleSerial.println(val);
-    //Wait for 0.1 second
+  // If bluetooth is connected
+  if (bleSerial.connected())
+  {
+    // Send the value
+    bleSerial.println(counter);
+    // Wait for 0.1 second
     delay(100);
   }
+}
+
+void updateCounter()
+{
+  // compare the current state to previous state
+  if (state != previousState && state)
+  {
+    // if they are differnt, and current state is high, then the wheel has turned:
+    counter++;
+    Serial.println(counter);
+  }
+  // save the current state as the last state, for next time through the loop
+  previousState = state;
 }
